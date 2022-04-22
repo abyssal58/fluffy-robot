ab9e4432d8b407d483ac9bcce0dd6b5aff754d14
abyssal58 <103748613+abyssal58@users.noreply.github.com>
2022-04-23 00:47:24 +0530

    Add files via upload

---
+++ assignment 1 solns.py
@@ -0,0 +1,31 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================================================================== RESTART: D:/OneDrive/Desktop/assignment1.py =====================================================================
+QUESTION 1 :
+ENTER A NUMBER : 10
+ENTER ANOTHER NUMBER : 20
+ENTER ANOTHER NUMBER : 30
+20.0
+QUESTION 2 :
+ENTER GROSS INCOME : 500000
+ENTER NUMBER OF DEPENDENTS : 4
+95600.0
+QUESTION 3 :
+ENTER YOUR SID : 21102085
+ENTER YOUR NAME : Arnav Sharma
+ENTER YOUR GENDER : male
+ENTER YOUR COURSE NAME : ES1101
+ENTER YOUR CGPA 8.5
+[21102085, 'Arnav Sharma', 'male', 'ES1101', 8.5]
+QUESTION 4 :
+ENTER MARKS OF FIRST STUDENT 59
+ENTER MARKS OF SECOND STUDENT 76
+ENTER MARKS OF THIRD STUDENT53
+ENTER MARKS OF FOURTH STUDENT 82
+ENTER MARKS OF FIFTH STUDENT96
+[53, 59, 76, 82, 96]
+QUESTION 5 :
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+PART B 
+['Red', 'Green', 'White', 'Purple', 'Yellow']
