5c04fd422778599c5449a4412a726c34174e9585
abyssal58 <103748613+abyssal58@users.noreply.github.com>
2022-04-22 23:55:46 +0530

    Add files via upload

---
+++ assignment1solution.py
@@ -0,0 +1,30 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: D:/OneDrive/Desktop/assignment1.py =================
+QUESTION 1 :
+ENTER A NUMBER : 34
+ENTER ANOTHER NUMBER : 45
+ENTER ANOTHER NUMBER : 77
+52.0
+QUESTION 2 :
+ENTER GROSS INCOME : 400000
+ENTER NUMBER OF DEPENDENTS : 3
+76200.0
+QUESTION 3 :
+ENTER YOUR SID : 21102085
+ENTER YOUR NAME : Arnav Sharma
+ENTER YOUR GENDER : male
+ENTER YOUR COURSE NAME : ES1101
+ENTER YOUR CGPA9.8
+[21102085, 'Arnav Sharma', 'male', 'ES1101', 9.8]
+QUESTION 4 :
+ENTER MARKS OF FIRST STUDENT 23
+ENTER MARKS OF SECOND STUDENT 56
+ENTER MARKS OF THIRD STUDENT69
+ENTER MARKS OF FOURTH STUDENT 96
+ENTER MARKS OF FIFTH STUDENT100
+[23, 56, 69, 96, 100]
+QUESTION 5 :
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
